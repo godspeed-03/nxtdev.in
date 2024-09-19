ebab40d6b5ed7f98bf36e0cf0b7a170bcc498ac1
godspeed-03 <satyam2001anand@gmail.com>
2024-09-19 22:58:43 +0530

    Init : intialised codebase

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import React from 'react'
 <div className="w-full h-screen flex flex-col justify-center items-center text-white gap-5">
 
   <h1 className='font-bold text-5xl'>Coming Soon ...................</h1>
-  <h2 className='font-bold text-3xl'>Revamping under development........</h2>
+  <h2 className='font-bold text-3xl'>Revamping & Under development........</h2>
   <p>Refer to following figma link :</p>
   <a href="https://www.figma.com/design/sbkJIojAmC1bjReEvlQXJl/Portfolio-v_1?node-id=0-1&node-type=canvas&t=2BAe1kQHBeNnMf95-0" target='_blank'>
     <p className='hover:text-red-800 text-blue-600'>Link</p>
